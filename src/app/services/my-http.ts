d8519a2b73d33c285df7aba7bad84650fab813a0
Alessio Coci <G00474271@atu.ie>
2026-04-19 17:13:12 +0000

    Services implemented and storage registered in main.ts

--- src/app/services/my-http.ts
+++ src/app/services/my-http.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import {CapacitorHttp, HttpOptions} from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MyHttp {
-  
+export class MyHttpService {
+  constructor() { }
+
+  async get (options: HttpOptions) {
+    return await CapacitorHttp.get(options);
+  }
 }
