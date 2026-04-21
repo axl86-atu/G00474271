eb5c8fefd6ffce28eec26f2c13ee0d122ee1143c
Alessio Coci <G00474271@atu.ie>
2026-04-21 16:04:26 +0000

    cast and crew on movie details, and "No image" when img not present

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonAvatar, IonLabel } from '@ionic/angular/standalone';
 import { MyDataService } from '../services/my-data';
+import { MyHttpService } from '../services/my-http';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonAvatar, IonLabel]
 })
 export class MovieDetailsPage {
   movie:any = {}; //hold the movie object we read back from storage
+  cast: any[] = [];
+  crew: any[] = [];
+  API_KEY = '806536462a0bda2adf6e3b5c2e6b1aed';
 
-  constructor(private mds: MyDataService) { 
+  constructor(private mds: MyDataService, private mhs: MyHttpService) { 
 
   }
 
@@ -26,6 +31,18 @@ export class MovieDetailsPage {
   async loadMovie() {
     this.movie = await this.mds.get('selectedMovie');
     console.log(this.movie); //checking if movie obj's been captured
+    await this.loadCredits();
+  }
+
+  async loadCredits() {
+    let options: HttpOptions = {
+      url: 'https://api.themoviedb.org/3/movie/' + this.movie.id + '/credits?api_key=' + this.API_KEY
+    };
+    let result = await this.mhs.get(options);
+    this.cast = result.data.cast;
+    this.crew = result.data.crew;
+    console.log('Cast:', this.cast);
+    console.log('Crew:', this.crew);
   }
 
 }
