c760d2d5e67a13bfa13c4b8fb492c8aefd6fb8da
Alessio Coci <G00474271@atu.ie>
2026-04-22 10:49:42 +0000

    added home icon to Favourites page

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { MyDataService } from '../services/my-data';
 import { Router } from '@angular/router';
+import {addIcons} from 'ionicons';
+import {home} from 'ionicons/icons';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonButtons, IonIcon]
 })
 export class FavouritesPage implements OnInit {
   favourites: any[] = [];
 
-  constructor(private mds: MyDataService, private router: Router) { }
+  constructor(private mds: MyDataService, private router: Router) { 
+    addIcons({ home });
+  }
 
   ngOnInit() { }
 
@@ -31,5 +35,9 @@ export class FavouritesPage implements OnInit {
   async loadFavourites() {
     this.favourites = await this.mds.getFavourites();
     console.log(this.favourites);
+  }
+
+  goHome() {
+    this.router.navigate(['/home']);
   }
 }
